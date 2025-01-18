446cbf948ac611a0ef0af3745bbda15f4a82c8f6
Aryan Bhatia <abtgofficial@gmail.com>
2025-01-18 19:01:03 +0530

    added Input Polling

---
+++ Elysium/src/Platform/Windows/WindowsInput.cpp
@@ -0,0 +1,37 @@
+#include "core/Input.h"
+
+#include "core/Application.h"
+
+#include <GLFW/glfw3.h>
+
+bool Input::IsKeyPressed(const KeyCode key)
+{
+	auto* window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
+	auto state = glfwGetKey(window, static_cast<int32_t>(key));
+	return state == GLFW_PRESS;
+}
+
+bool Input::IsMouseButtonPressed(const MouseCode button)
+{
+	auto* window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
+	auto state = glfwGetMouseButton(window, static_cast<int32_t>(button));
+	return state == GLFW_PRESS;
+}
+
+glm::vec2 Input::GetMousePosition()
+{
+	auto* window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
+	double xPos, yPos;
+	glfwGetCursorPos(window, &xPos, &yPos);
+	return { (float)xPos, (float)yPos };
+}
+
+float Input::GetMouseX()
+{
+	return GetMousePosition().x;
+}
+
+float Input::GetMouseY()
+{
+	return GetMousePosition().y;
+}
